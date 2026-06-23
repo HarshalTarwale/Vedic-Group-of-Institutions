935e045b17714ad16d097be7456005e8a1186d2a
it-techmonkey <adityahazari14@gmail.com>
2026-06-24 01:20:10 +0530

    FIxed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Vedic-Group-of-Institutions/',
+  base: './',
 })
